9c9cacccbfc3a7cc439bec27c23b6f1f571b435a
Attila Juhász-Nagy <juhasznagyattila@gmail.com>
2014-03-26 05:39:46 +0000

    + EXIF kezelő

---
+++ PhotoReorder/ExifInformer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing.Imaging;
+
+namespace PhotoReorder
+{
+    // http://msdn.microsoft.com/en-us/library/xddt0dz7.aspx
+    // http://stackoverflow.com/questions/58649/how-to-get-the-exif-data-from-a-file-using-c-sharp
+
+    class ExifInformer
+    {
+
+    }
+}
