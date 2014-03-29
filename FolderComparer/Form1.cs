2142a50eebcaf6e6f7f58cd1510c6ebc3fc2f105
Attila Juhász-Nagy <juhasznagyattila@gmail.com>
2014-03-29 00:49:51 +0000

    * könyvtárból az alkönyvtárak listázása

--- FolderComparer/Form1.cs
+++ FolderComparer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,35 @@ namespace FolderComparer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var _folder = GetFolder();
+            //MessageBox.Show(_folder);
+            var di = new DirectoryInfo(_folder);
+
+            if (di == null) return;
+
+            textBox1.Text = "";
+            foreach (var x in di.EnumerateDirectories())
+            {
+                textBox1.Text += x.Name + Environment.NewLine;
+            }
+        }
+
+        private string GetFolder()
+        {
+            var result = "";
+
+            var fod = new FolderBrowserDialog();
+            fod.ShowNewFolderButton = false;
+            if (fod.ShowDialog() == DialogResult.OK)
+            {
+                result = fod.SelectedPath;
+            }
+
+            return result;
         }
     }
 }
